1024042af232c7b41e5c32c66c07cd004d127c08
2019107032103PaulMahoney <HeatherBarrero803@outlook.com>
2025-12-04 22:33:46 +0800

    add UniversalAdapter__factory.ts, WalletSelector.tsx, validator-spec-responsive.jsx

---
+++ queries/download-router-adaptive/validator-spec-responsive.jsx
@@ -0,0 +1,3 @@
+export const config = { runtime: 'edge' };
+
+export default function mock-util() { return <div>Component</div>; }
---
+++ sms.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\sms.go
+// Generated at: 2025-10-16T06:30:52.962Z
